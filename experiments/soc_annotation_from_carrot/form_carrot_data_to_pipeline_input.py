ae2f9143e27ed6957a4e7632fddf45b99e0d0f83
rictuar <parkerbremer@gmail.com>
2022-02-04 21:30:15 -0800

    initial commit

---
+++ experiments/soc_annotation_from_carrot/form_carrot_data_to_pipeline_input.py
@@ -0,0 +1,147 @@
+#this program takes the output from carrot and transforms it into something pretty close to what we got from the orginal 
+#binvestigate pull.
+#the basic idea is to obtain the total list of compounds and the total list of (species, organ)
+#then we check the soc annotation-only files for each species organ compound
+#if the file is found we impute whats missing and add to the input panda
+#if the file isnt found we impute the entire list using the noise level
+#somewhat unfortunately, retrospectively, the information is stored in "parallel lists" for each column for each bin
+#so, the length of species, organ, count, median intensity, annotation distribution, etc should be equal 
+
+import pandas as pd
+import numpy as np
+
+
+def get_compound_properties(compound_properties_panda):
+
+    a=list(zip(
+            compound_properties_panda.loc[compound_properties_panda.target_type=='CONFIRMED'].target_id.astype(int),
+            compound_properties_panda.loc[compound_properties_panda.target_type=='CONFIRMED'].name
+    ))
+    unique_bin_and_name=list()
+    [unique_bin_and_name.append(x) for x in a if x not in unique_bin_and_name]
+
+    return unique_bin_and_name
+
+    # a=np.sort(
+    #     np.array(list(zip(
+    #         compound_properties_panda.loc[compound_properties_panda.target_type=='CONFIRMED'].target_id.astype(float),
+    #         compound_properties_panda.loc[compound_properties_panda.target_type=='CONFIRMED'].name
+    #     ))),axis=0
+    # )
+    #print(a)
+
+def get_all_species_organs():
+    pass
+
+def make_empty_input_panda(pipeline_input_panda_columns,bin_and_name_list):
+    temp_dict_of_list={
+        temp_column:[np.nan for i in range(len(bin_and_name_list))] for temp_column in pipeline_input_panda_columns
+    }
+
+    temp_dict_of_list['id']=[temp[0] for temp in bin_and_name_list]
+    temp_dict_of_list['name']=[temp[1] for temp in bin_and_name_list]
+
+    temp_dict_of_list['species']=[list() for temp in bin_and_name_list]
+    temp_dict_of_list['organ']=[list() for temp in bin_and_name_list]
+    temp_dict_of_list['median_intensity']=[list() for temp in bin_and_name_list]
+    temp_dict_of_list['annotation_distribution']=[list() for temp in bin_and_name_list]
+    temp_dict_of_list['count']=[list() for temp in bin_and_name_list]
+
+    pipeline_input_panda=pd.DataFrame.from_dict(temp_dict_of_list)
+
+    # print(pipeline_input_panda)
+    return pipeline_input_panda
+
+def insert_combination(species,organ,compound):
+    pass
+
+def impute_annotations_at_least_one_found(temp_annotations_list,temp_so_count):
+    percent_present=len(temp_annotations_list)/temp_so_count
+    imputed_value=percent_present*min(temp_annotations_list)
+    return temp_annotations_list+[imputed_value for i in range(temp_so_count-len(temp_annotations_list))]
+
+def impute_annotations_zero_found(temp_average_fame_intensity,temp_so_count):
+    imputed_value=noise_intensity/temp_average_fame_intensity
+    return [imputed_value for i in range(temp_so_count)]
+
+def insert_combination_wrapper(pipeline_input_panda,bin_and_name_list,species_organ_pair_list,data_base_address,species_organ_properties_panda):
+    #
+
+    for temp_bin_and_name in bin_and_name_list:
+        print(temp_bin_and_name)
+
+        for temp_species_organ_pair in species_organ_pair_list:
+
+            this_species_organ_count=species_organ_properties_panda.loc[
+                (species_organ_properties_panda.species==temp_species_organ_pair[0]) &
+                (species_organ_properties_panda.organ==temp_species_organ_pair[1])
+            ]['count'].item()
+
+            try:
+                temp_panda=pd.read_pickle(data_base_address+temp_species_organ_pair[0]+'¬'+temp_species_organ_pair[1]+'¬'+str(temp_bin_and_name[0])+'.bin')
+                
+                
+                temp_imputed_annotations_list=impute_annotations_at_least_one_found(temp_panda['normalized_intensity'].to_list(),this_species_organ_count)
+
+
+                # print(temp_panda['normalized_intensity'].to_list())
+                # print(temp_imputed_annotations_list)
+                # print(this_species_organ_count)
+                # print(len(temp_panda['normalized_intensity'].to_list()))
+                # print(len(temp_imputed_annotations_list))
+                
+            except FileNotFoundError:
+                this_species_organ_average_fame_intensity=species_organ_properties_panda.loc[
+                    (species_organ_properties_panda.species==temp_species_organ_pair[0]) &
+                    (species_organ_properties_panda.organ==temp_species_organ_pair[1])
+                ]['average_fame_intensity'].item()
+
+                temp_imputed_annotations_list=impute_annotations_zero_found(this_species_organ_average_fame_intensity,this_species_organ_count)
+
+                
+                # print(temp_imputed_annotations_list)
+                # print(this_species_organ_count)
+                # print(this_species_organ_average_fame_intensity)
+                # hold=input('hold')
+
+            pipeline_input_panda.at[temp_bin_and_name[0],'species'].append(temp_species_organ_pair[0])
+            pipeline_input_panda.at[temp_bin_and_name[0],'organ'].append(temp_species_organ_pair[1])
+            pipeline_input_panda.at[temp_bin_and_name[0],'count'].append(this_species_organ_count)
+            pipeline_input_panda.at[temp_bin_and_name[0],'median_intensity'].append(np.median(temp_imputed_annotations_list))
+            pipeline_input_panda.at[temp_bin_and_name[0],'annotation_distribution'].append(temp_imputed_annotations_list)
+
+    return pipeline_input_panda            
+
+
+if __name__=="__main__":
+    
+    noise_intensity=200
+    data_base_address='./soc_data/'
+    pipeline_input_panda_columns=['id','name','species','organ','count','median_intensity','group','inchikey','annotation_distribution']
+    compound_properties_panda_address='./so_count_data/all_species_organs_compounds_panda.bin'
+    species_organ_properties_panda_address='./so_count_data/species_organ_sample_count_and_average_fame.bin'
+    output_address='./pipeline_input/pipeline_input.bin'
+    
+    compound_properties_panda=pd.read_pickle(compound_properties_panda_address)
+    species_organ_properties_panda=pd.read_pickle(species_organ_properties_panda_address)
+    
+
+    #bin_list=get_all_bins()
+    #species_organ_list=get_all_species_organs()
+
+    bin_and_name_list=get_compound_properties(compound_properties_panda)
+
+
+    species_organ_pair_list=list(zip(species_organ_properties_panda.species,species_organ_properties_panda.organ))
+    # print(species_organ_pair_list)
+
+    pipeline_input_panda=make_empty_input_panda(pipeline_input_panda_columns,bin_and_name_list)
+    pipeline_input_panda.set_index(keys='id',drop=False,inplace=True)
+    # print(pipeline_input_panda)
+    # hold=input('hold')
+    
+    pipeline_input_panda=insert_combination_wrapper(pipeline_input_panda,bin_and_name_list,species_organ_pair_list,data_base_address,species_organ_properties_panda)
+    pipeline_input_panda.reset_index(inplace=True,drop=True)
+
+    print(pipeline_input_panda)
+    pipeline_input_panda.to_pickle(output_address)
